661d1171d844e8dd3cbfa79a050044b6c633ec01
Tim Leslie <timl@thinkmill.com.au>
2020-07-02 15:32:41 +1000

    Remove support for multiple database adapters

--- packages/keystone/lib/Keystone/index.js
+++ packages/keystone/lib/Keystone/index.js
@@ -34,9 +34,7 @@ const { CustomProvider, ListAuthProvider, ListCRUDProvider } = require('../provi
 module.exports = class Keystone {
   constructor({
     defaultAccess,
-    adapters,
     adapter,
-    defaultAdapter,
     name,
     onConnect,
     cookieSecret,
@@ -83,12 +81,8 @@ module.exports = class Keystone {
       }),
     ];
 
-    if (adapters) {
-      this.adapters = adapters;
-      this.defaultAdapter = defaultAdapter;
-    } else if (adapter) {
-      this.adapters = { [adapter.constructor.name]: adapter };
-      this.defaultAdapter = adapter.constructor.name;
+    if (adapter) {
+      this.adapter = adapter;
     } else {
       throw new Error('No database adapter provided');
     }
@@ -266,8 +260,7 @@ module.exports = class Keystone {
   }
 
   createList(key, config, { isAuxList = false } = {}) {
-    const { getListByKey, adapters } = this;
-    const adapterName = config.adapterName || this.defaultAdapter;
+    const { getListByKey, adapter } = this;
     const isReservedName = !isAuxList && key[0] === '_';
 
     if (isReservedName) {
@@ -282,7 +275,7 @@ module.exports = class Keystone {
       composePlugins(config.plugins || [])(config, { listKey: key, keystone: this }),
       {
         getListByKey,
-        adapter: adapters[adapterName],
+        adapter,
         defaultAccess: this.defaultAccess,
         registerType: type => this.registeredTypes.add(type),
         isAuxList,
@@ -439,9 +432,7 @@ module.exports = class Keystone {
    * constructor, or `undefined` if no `onConnect` method specified.
    */
   async connect() {
-    const { adapters } = this;
-    const rels = this._consolidateRelationships();
-    await resolveAllKeys(mapKeys(adapters, adapter => adapter.connect({ rels })));
+    await this.adapter.connect({ rels: this._consolidateRelationships() });
 
     if (this.eventHandlers.onConnect) {
       return this.eventHandlers.onConnect(this);
@@ -452,7 +443,7 @@ module.exports = class Keystone {
    * @return Promise<null>
    */
   async disconnect() {
-    await resolveAllKeys(mapKeys(this.adapters, adapter => adapter.disconnect()));
+    await this.adapter.disconnect();
   }
 
   getAdminMeta({ schemaName }) {
